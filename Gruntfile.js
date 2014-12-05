7bcf29fcf97af2abc04e48b1ac7e1d67f9366844
Zoe Ames <zoeames@gmail.com>
2014-12-05 12:29:02 -0600

    analytics added

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt){
     // ---------------------------------------------------------------------- //
     jshint: {
       options: {jshintrc: '.jshintrc', reporter: require('jshint-stylish')},
-      all: ['Gruntfile.js', 'client/**/*.js', 'server/**/*.js', 'test/**/*.js']
+      all: ['Gruntfile.js', 'client/**/*.js', 'server/**/*.js', 'test/**/*.js', '!client/analytics.js']
     },
     // ---------------------------------------------------------------------- //
     jscs: {
